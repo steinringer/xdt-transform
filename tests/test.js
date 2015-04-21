43a58274be5d58a29173c59c9c301a2255654b82
Jozef Lacny <jozef.lacny@steinringer.sk>
2015-04-21 10:33:49 +0200

    npm package created

---
+++ tests/test.js
@@ -0,0 +1,4 @@
+﻿var XdtTransform = require('../xdt-transform.js');
+
+var transformer = new XdtTransform();
+transformer.transform('tests/Web.config', 'tests/Web.Transform.config', 'tests/Web.config');
---
+++ xdt-transform.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+
+var path = require('path'),
+    exec = require('child_process').exec;
+var ctt = 'bin/ctt.exe';
+
+var XdtTransform = function () { };
+
+XdtTransform.prototype.transform = function (source, transform, destination) {
+    /// <summary>
+    /// Transforms XML using XDT transformation (ctt.exe is used)
+    /// </summary>
+    /// <param name="source">path to source XML file</param>
+    /// <param name="transform">path to transformation XML file</param>
+    /// <param name="destination">path where output file should be written (can be the same as source)</param>
+    /// <example> transform('[path-to-somewhere]\Web.config', '[path-to-somewhere]\Web.Transform.config', '[path-to-somewhere]\Web.config');</example>
+    
+    ctt = path.join(__dirname, ctt);
+    
+    var s = '"s:##"'.replace("##", source);
+    var t = '"t:##"'.replace("##", transform);
+    var d = '"d:##"'.replace("##", destination);
+    var args = [ctt, s, t, d, "pw"];
+    console.log(args.join(" "));
+    
+    exec(args.join(" "), function (error, stdout, stderr) {
+        if (error) console.log(error);
+    });
+}
+
+
+module.exports = XdtTransform;

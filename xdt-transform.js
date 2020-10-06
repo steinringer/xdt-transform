300acde2527c68065f892127368a940ee37fdcd3
kam <milos.kamensky@steinringer.sk>
2020-10-06 12:42:55 +0200

    feat(xdt-transform.js): add parameter "i"

--- xdt-transform.js
+++ xdt-transform.js
@@ -4,9 +4,9 @@ var path = require('path'),
     exec = require('child_process').exec;
 var cttExe = 'bin/ctt.exe';
 
-var XdtTransform = function () { };
+var xdtTransform = function () { };
 
-XdtTransform.prototype.transform = function (source, transform, destination, options, callback) {
+xdtTransform.prototype.transform = function(source, transform, destination, options, callback) {
     /// <summary>
     /// Transforms XML using XDT transformation (ctt.exe is used)
     /// </summary>
@@ -15,29 +15,35 @@ XdtTransform.prototype.transform = function (source, transform, destination, opt
     /// <param name="destination">path where output file should be written (can be the same as source)</param>
     /// <param name="options">specify {quiet: true} to hide the executable with arguments</param>
     /// <example> transform('[path-to-somewhere]\Web.config', '[path-to-somewhere]\Web.Transform.config', '[path-to-somewhere]\Web.config');</example>
-    
+
     options = options || {};
-    callback = callback || function () { };
-    
+    callback = callback || function() {};
+
     var cttPath = path.join(__dirname, cttExe);
     cttPath = '"' + cttPath + '"';
-    
+
+        //s: marks the source file.
+        //t: marks the transform file.
+        //d: marks the destination file.
+        //i: causes the output to be indented, because by default it is minified and unreadable.
+
     var s = '"s:##"'.replace("##", source);
     var t = '"t:##"'.replace("##", transform);
     var d = '"d:##"'.replace("##", destination);
-    var args = [cttPath, s, t, d, "pw"];
+    var args = [cttPath, s, t, d, "pw", "i"];
     if (!options.quiet) {
         console.log(args.join(" "));
     }
-    
-    exec(args.join(" "), function (error, stdout, stderr) {
-        if (error) {
-            console.log(error);
-        } else {
-            callback();
-        }
-    });
-}
+
+    exec(args.join(" "),
+        function(error, stdout, stderr) {
+            if (error) {
+                console.log(error);
+            } else {
+                callback();
+            }
+        });
+};
 
 
-module.exports = XdtTransform;
+module.exports = xdtTransform;
